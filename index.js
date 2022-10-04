57cd1a41b3172f579f460cf1ef30c97ecbfb54e0
Gerald <kapgerado05@gmail.com>
2022-10-04 15:24:32 +0300

    Done with first-class-function assignment

--- index.js
+++ index.js
@@ -8,7 +8,17 @@ let returnsANamedFunction = (x='Already def fxn') => {
   }
   return defFxn;
 }
-
 let returnsAnAnonymousFunction = () => {
   return () => {};
+}
+function returnsAnAnonymousFunction(){
+  let myComp = ['hp', 'DELL', 'Lenovo'] 
+  let keyLights;
+  if(myComp=== 'hp'){
+    keyLights='supper bright';
+  }
+  else if(myComp==='DELL'){
+    keyLights='Some What Black!';
+  }
+  else {keyLights='Yellow'}
 }
